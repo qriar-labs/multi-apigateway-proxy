6e36d04f993e74e695e22f4a970b63671717310e
Edgar Silva <edgar@skalena.com>
2023-09-06 16:26:04 -0300

    fixing

---
+++ src/test/java/com/qriarlabs/qap/multigateway/MultigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qriarlabs.qap.multigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
